eb0f0453322d581068aae5e937d18f45bd81f95b
Loc20904 <161731538+Loc20904@users.noreply.github.com>
2024-09-21 20:55:48 +0700

    Update javaS.js

--- javaS.js
+++ javaS.js
@@ -1,6 +1,6 @@
 
 function changeImage(id)
 {
-  document.getElementById('id').src = "talkingphoto1.jpeg";
+  document.getElementsById('id').src = "talkingphoto1.jpeg";
   document.getlementsByTagName('a').innerHTML = "Text in here had change";
 }

a75a55f2fc65b81ab6beddc1c51f6cd980f6e5e4
Loc20904 <161731538+Loc20904@users.noreply.github.com>
2024-09-21 21:13:42 +0700

    Update javaS.js

--- javaS.js
+++ javaS.js
@@ -1,6 +1,6 @@
 
 function changeImage()
 {
-  document.getElementById('imgP1').src = "takingphoto1.jpeg";
+  document.getElementById('imgP1').src = "https://github.com/Loc20904/CSS_courese1/blob/main/takingphoto1.jpeg";
   
 }

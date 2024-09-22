edfa5bcb9934bc1fc795ff0773a78074deb5a776
Loc20904 <161731538+Loc20904@users.noreply.github.com>
2024-09-22 13:55:48 +0700

    Update javaS.js

--- javaS.js
+++ javaS.js
@@ -4,3 +4,11 @@ function changeImage()
   document.getElementById('imgP1').src = "takingphoto4.jpg";
   
 }
+
+function randomImg(id){
+	const arrImg =["image/hobbies/takingphoto1.jpeg","image/hobbies/takingphoto3.jpeg","image/hobbies/takingphoto2.jpg","image/hobbies/takingphoto4.jpg"];
+
+	random = arrImg[Math.floor(Math.random()* arrImg.length)];
+
+	document.getElementById("id").src = random;
+}
